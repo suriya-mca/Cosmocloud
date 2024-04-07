cafc18f26071fc2945ac4378f880a2563eceb3a9
Suriya <suriyag1999@gmail.com>
2024-04-08 00:47:29 +0530

    get all students end-point implemented

--- app/router/lms_router.py
+++ app/router/lms_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from model.models import Student
-from service.lms_service import create_student_service
+from service.lms_service import create_student_service, get_students_service
 
 
 router = APIRouter()
@@ -9,3 +9,9 @@ router = APIRouter()
 async def create_student(student: Student):
     
     return await create_student_service(student)
+
+@router.get("/students", status_code=200, tags=["lms"])
+async def get_students(country: str = None, age: int = None):
+
+    return await get_students_service(country, age)
+    
--- app/service/lms_service.py
+++ app/service/lms_service.py
@@ -6,3 +6,14 @@ async def create_student_service(student: Student):
 
 	result = collection.insert_one(student.dict())
 	return {"id": str(result.inserted_id)}
+
+async def get_students_service(country, age):
+
+	filters = {}
+	if country:
+		filters["address.country"] = country
+	if age:
+		filters["age"] = {"$gte": age}
+
+	students = list(collection.find(filters, {"_id": 0, "name": 1, "age": 1}))
+	return {"data": [dict(student) for student in students]}
